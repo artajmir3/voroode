22a2f0b6ae7c146570f7e69fd7e3b63c52c310d1
artajmir3 <artajmir3@yahoo.com>
2022-10-03 00:18:37 -0700

    major

--- herokuapp/admin.py
+++ herokuapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from herokuapp.models import *
 
 # Register your models here.
+
+
+class SusAdmin(admin.ModelAdmin):
+    pass
+
+
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Suspects, SusAdmin)
+admin.site.register(State, StateAdmin)
+
